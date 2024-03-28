0ec0ccabd9fd1e1b94f48f00cf19a658c481cbd2
fayekelmith <faye.12222883@lpu.in>
2024-03-29 01:12:19 +0530

    ready to merge

--- dockers.py
+++ dockers.py
@@ -2,12 +2,11 @@
 # definition of docker properties here
 import docker 
 class Image:
-    def __init__(self, name):
+    def __init__(self):
         self.client = docker.from_env()
-        self.name = name
     
     
-    def display_all_images(self,)-> list:
+    def display_all_images(self,all:bool = True)-> list:
         return self.client.images.list()
         
     def display_image(self, image_name: str) -> str:
@@ -19,3 +18,14 @@ class Image:
     def push_image(self, repository:str, tag:str = None) -> str:
         return self.client.images.push(repository, tag)
     
+    
+    
+# for formating time
+def truncate_microseconds(timestamp: str) -> str:
+    # Split the timestamp into date, time, and microseconds
+    date, time, microseconds = timestamp[:-1].split("T")[0], timestamp[:-1].split("T")[1].split(".")[0], timestamp[:-1].split("T")[1].split(".")[1]
+    # Truncate the microseconds to 6 decimal places
+    truncated_microseconds = microseconds[:6]
+    # Combine the date, time, and truncated microseconds
+    truncated_timestamp = f"{date}T{time}.{truncated_microseconds}Z"
+    return truncated_timestamp
