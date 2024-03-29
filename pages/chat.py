fbffa0df00dd514543428793abd9e6047f9ee9f9
fayekelmith <faye.12222883@lpu.in>
2024-03-29 10:45:41 +0530

    updated chats

--- pages/chat.py
+++ pages/chat.py
@@ -2,25 +2,28 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv()
 
 output_parser = StrOutputParser()
 
 
 st.title("We Transformed Flawed Docker Scripts to prestine lines")
 
+openai_api_key = ""
 
-
-llm = ChatOpenAI(openai_api_key='')
+llm = ChatOpenAI(openai_api_key=openai_api_key,temperature=0.8)
 
 
 with st.form('Docker Convo'):
-    prompt = ChatPromptTemplate.from_messages([("system", "You are world class Docker Expert. and your duty is to write the correct the given docker script to the best version with commented explanation"),("user", "{input}")])
+    prompt = ChatPromptTemplate.from_messages([("system", "You are world class Docker Expert. and your duty is to write the correct the given docker script to the best version with commented explanation. If input is irrelevant to docker, politely decline the request"),("user", "{input}")])
     
     chain = prompt | llm | output_parser
     
     text: str = st.text_area('Enter your Docker script')
     
-    submitted = st.form_submit_button('Correct Script')
+    submitted = st.form_submit_button('Improve Docker Script')
     
     if submitted:
         with st.spinner("Thinking... 🧠"): 
